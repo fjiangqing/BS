7f2b662e271b34703c14e45d7d7aa1c5f502f878
fjiangqing <fjiangqing@live.com>
2018-11-16 15:27:04 +0800

    first commit

--- mycv2.py
+++ mycv2.py
@@ -7,25 +7,10 @@ Created on Thu Jun  7 17:24:41 2018
 
 import cv2
 
-clicked = False
-
-def omMouse(event, x, y, flags, param):
-    global clicked
-    if event == cv2.EVENT_FLAG_LBUTTON:
-        clicked = True
 
 cameraCapture = cv2.VideoCapture(0)
 
-#cv2.namedWindow('myWindows')
-#cv2.setMouseCallback('myWindow', onMouse)
-
-#print('Showing camera feed. Click window or pres any to stop')
-
 success, frame = cameraCapture.read()
 
-while success and cv2.waitKey(1) == -1 and not clicked:
-    cv2.imshow("myWindow", frame)
-    success, frame = cameraCapture.read()
-    
-cv2.destroyAllWindows()
+cv2.imwrite("cat2.jpg", success)
 cameraCapture.release()

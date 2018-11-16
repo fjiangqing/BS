4697434f59fa56953f3958f5e632496b854e5441
fjiangqing <fjiangqing@live.com>
2018-11-16 16:49:17 +0800

    first commit

--- BenDi1.py
+++ BenDi1.py
@@ -6,77 +6,58 @@ import time
 import signal
 import atexit
 
-
-
-
-
+#设定GPIO 使用板物理引脚模式
 GPIO.setmode(GPIO.BOARD)
 
+#设定光敏传感器 
 lightPin = 7
 lightPin1 = 11
 
-
-GPIO.setup(lightPin1, GPIO.OUT)
-GPIO.output(lightPin1, GPIO.LOW)
+#设定引脚输入模式
+GPIO.setup(lightPin, GPIO.IN)
 time.sleep(0.5)
 GPIO.setup(lightPin1, GPIO.IN)
 
-
-GPIO.setup(lightPin, GPIO.OUT)
-GPIO.output(lightPin, GPIO.LOW)
-time.sleep(0.5)
-GPIO.setup(lightPin, GPIO.IN)
-
-
+#土壤检测传感器
 turan = 13
-
-GPIO.setup(turan, GPIO.OUT)
-GPIO.output(turan, GPIO.LOW)
-time.sleep(0.5)
 GPIO.setup(turan, GPIO.IN)
-
-#atexit.register(GPIO.cleanup)
+time.sleep(0.5)
 
 relay = 15
-
-GPIO.setwarnings(False)
-# =============================================================================
-# GPIO.setmode(GPIO.BCM)
-# =============================================================================
 GPIO.setup(relay, GPIO.OUT)
 
-servopin = 29
+#设定舵机控制
+servopin = 40
 GPIO.setup(servopin, GPIO.OUT, initial=False)
-#50HZ  
+#设定频率50HZ  
 p = GPIO.PWM(servopin, 50)
-
 p.start(0)
 time.sleep(2)
 
-
 while(True):
-
-   t = GPIO.input(turan)
-   if (t == GPIO.LOW):
-      GPIO.output(relay, GPIO.HIGH)
-      time.sleep(1)
-   else:
-       GPIO.output(relay, GPIO.LOW)
+    
+    #检测土壤湿度
+    t = GPIO.input(turan)
+    if (t == GPIO.LOW):
+        GPIO.output(relay, GPIO.HIGH)
+        time.sleep(1)
+    else:
+        GPIO.output(relay, GPIO.LOW)
        time.sleep(1)
-
-   v = GPIO.input(lightPin)
-   if (v == GPIO.LOW):
-      for i in range(0,180,10):
-         p.ChangeDutyCycle(2.5 + 10 * i/180)
-         time.sleep(0.02)
-         p.ChangeDutyCycle(0)
-         time.sleep(0.2)
-   else:
-      v1 = GPIO.input(lightPin1)
-      if (v1 == GPIO.LOW):
-          for i in range(180,0,-10):
-              p.ChangeDutyCycle(2.5 + 10 * i/180)
-              time.sleep(0.02)
-              p.ChangeDutyCycle(0)
-              time.sleep(0.2)
+    
+    v = GPIO.input(lightPin)
+    if (v == GPIO.LOW):
+        for i in range(0,180,10):
+        p.ChangeDutyCycle(2.5 + 10 * i/180)
+        time.sleep(0.02)
+        p.ChangeDutyCycle(0)
+        time.sleep(0.2)
+    else:
+        v1 = GPIO.input(lightPin1)
+        if (v1 == GPIO.LOW):
+            for i in range(180,0,-10):
+                p.ChangeDutyCycle(2.5 + 10 * i/180)
+                time.sleep(0.02)
+                p.ChangeDutyCycle(0)
+                time.sleep(0.2)
 

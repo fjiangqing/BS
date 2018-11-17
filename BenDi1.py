ed551f83adf18acaf63a3bf1e773196bb6e90d9e
fjiangqing <fjiangqing@live.com>
2018-11-17 20:05:44 +0800

    first commit

--- BenDi1.py
+++ BenDi1.py
@@ -6,6 +6,19 @@ import time
 import signal
 import atexit
 
+# =============================================================================
+# #设定光敏传感器 
+# lightPin = 7
+# lightPin1 = 11
+# #土壤检测传感器
+# turan = 13
+# #继电器检测
+# relay = 15
+# #设定舵机控制
+# servopin = 40
+# =============================================================================
+
+
 #设定GPIO 使用板物理引脚模式
 GPIO.setmode(GPIO.BOARD)
 
@@ -23,9 +36,11 @@ turan = 13
 GPIO.setup(turan, GPIO.IN)
 time.sleep(0.5)
 
+#继电器检测
 relay = 15
 GPIO.setup(relay, GPIO.OUT)
 GPIO.output(relay, GPIO.LOW)
+
 #设定舵机控制
 servopin = 40
 GPIO.setup(servopin, GPIO.OUT, initial=False)
@@ -40,10 +55,10 @@ while(True):
     t = GPIO.input(turan)
     if (t == GPIO.LOW):
         GPIO.output(relay, GPIO.HIGH)
-        time.sleep(1)
+        time.sleep(0.1)
     else:
         GPIO.output(relay, GPIO.LOW)
-        time.sleep(1)
+        time.sleep(0.1)
     
     v = GPIO.input(lightPin)
     if (v == GPIO.LOW):
